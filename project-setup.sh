b0695cb8e1393fad32fb92ce22f986d8cae26f19
Maneesh S K <158586324+Maneesh-s-k@users.noreply.github.com>
2025-04-30 15:19:42 +0530

    latest (replace)

--- project-setup.sh
+++
@@ -1,9 +0,0 @@
-# Create a new React project
-npx create-react-app carbon-trading-platform
-cd carbon-trading-platform
-
-# Install additional dependencies
-npm install ethers web3 @heroicons/react tailwindcss postcss autoprefixer
-
-# Setup Tailwind CSS
-npx tailwindcss init -p
